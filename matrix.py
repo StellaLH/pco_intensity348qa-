ec2627110cc3a2f7937055c5e75c9864b4a0e959
StellaLH <sh16g14@soton.ac.uk>
2017-08-17 15:32:25 +0100

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -13,62 +13,81 @@ from scipy.optimize import curve_fit
 # exp_time=[0.1,0.2,0.5]
 
 ## intensities
-# for exposrue times 0.1 & 0.2
-intensities=['320000','100000','032000','010000','003200','001000','000320','000100']
-# for exposure time 0.5
-#intensities=['100000','032000','010000','003200','001000','000320','000100']
-#datasheet_ints=[0.39,0.1,0.039,0.008,0.00312,0.0008,0.000312,0.00008]
-datasheet_ints=[0.35,0.098,0.0343,0.007,0.00245,0.000784,0.0002401,0.000098] #for wavelengths 400-600
-other_filters=['sharif']
+
+#datasheet_ints=[0.35,0.098,0.0343,0.007,0.00245,0.000784,0.0002401,0.000098] #for wavelengths 400-600
+datasheet_ints=[0.35,0.098,0.0343,0.007,0.00245,0.000784,0.0002401] #for wavelengths 400-600
 
 filter_value=[]
 count=[]
-adjfilter=[]
-
-
 
 exp_time='0p1'
 final_array=np.empty((3,2048,2060))
+
+""" Open each file, find average intensity over the 100 frames """
+fid=h5py.File('/media/stella/F0B427B9B42780E8/Stella/exp_time0p1/intensity_0p320000/pco_000001.h5', 'r')
+data=fid['/entry/data']
+mean0p320000=np.mean(data, axis=0)
+print 'Average intensity at T = 0.32000 found'
+
+fid=h5py.File('/media/stella/F0B427B9B42780E8/Stella/exp_time0p1/intensity_0p100000/pco_000001.h5', 'r')
+data=fid['/entry/data']
+mean0p100000=np.mean(data, axis=0)
+print 'Average intensity at T = 0.10000 found' 
+
+fid=h5py.File('/media/stella/F0B427B9B42780E8/Stella/exp_time0p1/intensity_0p032000/pco_000001.h5', 'r')
+data=fid['/entry/data']
+mean0p032000=np.mean(data, axis=0)
+print 'Average intensity at T = 0.03200 found'
+
+fid=h5py.File('/media/stella/F0B427B9B42780E8/Stella/exp_time0p1/intensity_0p010000/pco_000001.h5', 'r')
+data=fid['/entry/data']
+mean0p010000=np.mean(data, axis=0)
+print 'Average intensity at T = 0.01000 found'
+
+fid=h5py.File('/media/stella/F0B427B9B42780E8/Stella/exp_time0p1/intensity_0p003200/pco_000001.h5', 'r')
+data=fid['/entry/data']
+mean0p003200=np.mean(data, axis=0)
+print 'Average intensity at T = 0.00320 found'
+
+fid=h5py.File('/media/stella/F0B427B9B42780E8/Stella/exp_time0p1/intensity_0p001000/pco_000001.h5', 'r')
+data=fid['/entry/data']
+mean0p001000=np.mean(data, axis=0)
+print 'Average intensity at T = 0.00100 found'
+
+fid=h5py.File('/media/stella/F0B427B9B42780E8/Stella/exp_time0p1/intensity_0p000320/pco_000001.h5', 'r')
+data=fid['/entry/data']
+mean0p000320=np.mean(data, axis=0)
+print 'Average intensity at T = 0.00032 found'
+
+""" Analyse each pixel"""
+count=[]
 for ypixel in range(2048):
     for xpixel in range(2060):
-        for i in range(len(intensities)):
-            """
-            Get mean value of the pixel at each filter value and append to counts
-            """
-            # lab filters
-            fid=h5py.File('/media/stella/F0B427B9B42780E8/Stella/exp_time%s/intensity_0p%s/pco_000001.h5' %(exp_time,intensities[i]),'r')
-            data=fid['/entry/data']
-            mean_frame=np.mean(data, axis=0)
-            filter_value.append(datasheet_ints[i])
-            count.append(mean_frame[ypixel, xpixel])
-            
-            sharif_filters=intensities[:-1]
-        for j in range(len(sharif_filters)):
-               fid=h5py.File('/media/stella/F0B427B9B42780E8/Stella/exp_time%s/sharif/intensity_0p%s/pco_000001.h5' %(exp_time,intensities[j]),'r')
-               data=fid['/entry/data']
-               mean_frame=np.mean(data, axis=0)
-               filter_value.append(datasheet_ints[j]*0.2)
-               count.append(mean_frame[ypixel, xpixel])
-            
-        # dark frame
-        fid=h5py.File('/media/stella/F0B427B9B42780E8/Stella/exp_time%s/dark/pco_000001.h5'%exp_time,'r')
-        data=fid['/entry/data']
-        mean_frame=np.mean(data, axis=0)
-        filter_value.append(0.0)
-        count.append(mean_frame[ypixel, xpixel])
-        
-        """
-        Find linear fit of the pixel and append gradient & intercept to a 2D matrix
-        """
-        fit=np.polyfit(filter_value, count,1)
-        #fit=fitx*line[0]+line[1] #equation for fit
-        line=np.poly1d(fit) #gives function for line of best fit
-        straight_coeff=np.corrcoef(line(filter_value), count)[0,1]
+        #append average pixel value to count
+        print 'pixel x=', xpixel, 'y=' ,ypixel
+        count.append(mean0p320000[ypixel, xpixel])
+        #print 'data 1 added to count'
+        count.append(mean0p100000[ypixel, xpixel])
+        #print 'data 2 added to count'
+        count.append(mean0p032000[ypixel, xpixel])
+        #print 'data 3 added to count'
+        count.append(mean0p010000[ypixel, xpixel])
+        #print 'data 4 added to count'
+        count.append(mean0p003200[ypixel, xpixel])
+        #print 'data 5 added to count'
+        count.append(mean0p001000[ypixel, xpixel])
+        #print 'data 6 added to count'
+        count.append(mean0p000320[ypixel, xpixel])
+        #print 'data 7 added to count'
+     
+        # fit count against data sheet filter values
+        fit= np.polyfit(datasheet_ints, count,1)
+        line=np.poly1d(fit)
+        corr=np.corrcoef(line(datasheet_ints), count)[0,1]
+        # add fit values to final array
         final_array[0,ypixel,xpixel]=fit[1] #intercept
         final_array[1,ypixel,xpixel]=fit[0] #gradient
         final_array[2,ypixel,xpixel]=straight_coeff #correlation coefficient
-
-np.save('pco_intensity_response', final_array)
-            
-
+        count=[]
+        
 
